32c50b7a5f63bd9aeef7ff9f3def53af703057a2
asanto <andreiafilipasanto@gmail.com>
2016-09-28 21:34:55 +0100

    BUTTON CODE TO GO TO THE NEXT ACTIVITY

--- app/src/main/java/com/example/asanto/helloworldapplication/HelloWorldActivity.java
+++ app/src/main/java/com/example/asanto/helloworldapplication/HelloWorldActivity.java
@@ -1,7 +1,10 @@
 package com.example.asanto.helloworldapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+
+        Button nextButton = (Button)findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HelloWorldActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
